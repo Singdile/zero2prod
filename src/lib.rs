1a4cbb18748999ae790e622fd2a1504d094b3cc5
singdile <singdile0709@gmail.com>
2025-12-19 09:40:37 +0800

    health_check端点检查完成

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,19 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 use std::net::TcpListener;
 ///异步函数，处理网络请求通常需要等待I/O,使用异步让出CPU资源
 async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().finish()//HttpResponse::OK() 获取一个HttpResponseBuilder,然后生成一个HttpResponse
+    HttpResponse::Ok().finish() //HttpResponse::OK() 获取一个HttpResponseBuilder,然后生成一个HttpResponse
 }
-
 
 ///服务器启动函数,接受address作为服务器绑定的socket地址
 pub fn run(address: TcpListener) -> Result<Server, std::io::Error> {
     //HttpServer::new()创建一个HTTP服务器实例bind()绑定IP:端口
     // run()将绑定成功的Httpserver 转换为一个运行中(监听端口)的server实例,类型:
     //use actix_web::dev::Server;
-    let server = HttpServer::new(|| {
-       App::new()
-        .route("/health_check", web::get().to(health_check))
-    })
-    .listen(address)?
-    .run();
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(address)?
+        .run();
 
     Ok(server)
-
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,8 @@
 // !tests/health_check.rs
-use zero2prod;
 use std::net::TcpListener;
+use zero2prod;
 #[tokio::test]
 async fn health_check_work() {
-
     //准备
     let address = spawn_app();
     //引入reaWest对应用程序执行http请求
@@ -11,22 +10,20 @@ async fn health_check_work() {
 
     //执行
     let response = client
-        .get(&format!("{}/health_check",&address))
+        .get(format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
 
-   //断言
+    //断言
     assert!(response.status().is_success());
-   assert_eq!(Some(0),response.content_length());
+    assert_eq!(Some(0), response.content_length());
 }
 
-
 //在后台启动某处的应用程序,将服务程序绑定的addr返回
-fn spawn_app() -> String{
+fn spawn_app() -> String {
     //首先获得系统绑定的socket地址
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
 
     //检查系统分配的端口号
     let port = listener.local_addr().unwrap().port();
@@ -34,8 +31,7 @@ fn spawn_app() -> String{
 
     //spawn创建一个tokio task,将server放在上面去执行，立即返回执行下面的代码
     // 通常下面的代码是同task 是没有什么关系的，但是如果有要用到task的返回结果，那么就会在需要的位置执行.await()
-    let _ = tokio::spawn(server);
-
+    tokio::spawn(server);
 
     //返回地址给调用者
     format!("http://127.0.0.1:{}", port)
