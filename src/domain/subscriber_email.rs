ad11c05ddf31c68e3e52cca2818d369ae10e0e89
singdile <singdile0709@gmail.com>
2026-01-06 15:36:06 +0800

    为SubscriberEmail 添加测试

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -15,3 +15,28 @@ impl AsRef<str> for SubscriberEmail {
         &self.0
     }
 }
+
+///单元测试
+#[cfg(test)]
+mod tests {
+    use super::SubscriberEmail;
+    use claim::assert_err;
+
+    #[test]
+    fn empty_email_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "wpc163.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@163.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+}
