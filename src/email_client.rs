a92da35208629af40f9a5abe38b75c8e1e72ff8c
singdile <singdile0709@gmail.com>
2026-01-13 16:54:18 +0800

    test: 增添对Emailclient的连接测试

--- src/email_client.rs
+++ src/email_client.rs
@@ -26,6 +26,43 @@ impl EmailClient {
         html_content: &str,
         text_content: &str,
     ) -> Result<(), String> {
-        todo!()
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        domain::SubscriberEmail,
+        email_client::{self, EmailClient},
+    };
+    use fake::faker::lorem::en::{Paragraph, Sentence};
+    use fake::{Fake, faker::internet::en::SafeEmail};
+    use wiremock::matchers::any;
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        //期望发送邮件到base_url
+        let mock_server = MockServer::start().await; //完整的Http服务器,使用一个随机可用的端口
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender); //将MockServer的URL传递
+
+        //加入MockServer的mock行为，当收到任何的请求都匹配，并且返回200的状态
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1) //表示测试期间，应该仅收到一个匹配的请求
+            .mount(&mock_server)
+            .await;
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        //执行
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        //断言判断
     }
 }
