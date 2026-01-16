598024da00b5ca6d446f6df0d63a822eef3bdf0c
singdile <singdile0709@gmail.com>
2026-01-16 09:41:55 +0800

    增添EmailClient对于服务器返回的错误测试

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,6 +1,9 @@
 //! src/email_client.rs
+use std::time::Duration;
+
 use crate::domain::SubscriberEmail;
-use reqwest::Client;
+use config::builder;
+use reqwest::{Client, Response, ClientBuilder};
 use secrecy::{ExposeSecret, Secret};
 use serde;
 ///邮件客户端,将状态存储到数据结构，将行为放在impl实现
@@ -17,9 +20,11 @@ impl EmailClient {
         sender: SubscriberEmail,
         authorization_token: Secret<String>,
     ) -> Self {
+	//设置默认的超时时间
+	let builder =  Client::builder().timeout(Duration::from_secs(10)); //调用ClientBuilder来配置Client
         Self {
             sender,
-            http_client: Client::new(),
+            http_client: builder.build().unwrap(),
             base_url,
             authorization_token,
         }
@@ -41,7 +46,7 @@ impl EmailClient {
             html_body: html_content,
             text_body: text_content,
         };
-        let _ = self
+        let outcome = self
             .http_client
             .post(&url)
             .header(
@@ -50,8 +55,9 @@ impl EmailClient {
             )
             .json(&request_body) //自动添加header， "Content-type" "application/json"
             .send()
-            .await?;
-        Ok(())
+            .await?
+            .error_for_status()?; //当response的状态码有问题的时候，返回err
+	Ok(())
     }
 }
 
@@ -69,13 +75,16 @@ struct SendEmailRequest<'a> {
 #[cfg(test)]
 mod tests {
     use crate::{domain::SubscriberEmail, email_client::EmailClient};
+    use actix_web::test;
     use fake::faker::lorem::en::{Paragraph, Sentence};
     use fake::{Fake, Faker, faker::internet::en::SafeEmail};
     use secrecy::Secret;
     use wiremock::Match;
     use wiremock::Request;
-    use wiremock::matchers::{header, header_exists, method, path};
+    use wiremock::matchers::{any,header, header_exists, method, path};
     use wiremock::{Mock, MockServer, ResponseTemplate};
+    use claim::{assert_ok,assert_err};
+    use std::time::Duration;
 
     struct SendEmailBodyMatcher;
 
@@ -98,30 +107,110 @@ mod tests {
             }
         }
     }
+    
+
+    ///服务器返回正确
     #[tokio::test]
-    async fn send_email_sends_the_expected_request() {
+    async fn send_email_succeeds_if_the_server_returns_200() {
         //期望发送邮件到base_url
         let mock_server = MockServer::start().await; //完整的Http服务器,使用一个随机可用的端口
         let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
         let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake())); //将MockServer的URL传递
 
         //加入MockServer的mock行为
-        Mock::given(header_exists("X-Postmark-Server-Token"))
-            .and(header("Content-Type", "application/json"))
-            .and(method("POST"))
-            .and(SendEmailBodyMatcher) //使用自己定义的匹配，来检查请求的json
-            .respond_with(ResponseTemplate::new(200))
-            .expect(1) //表示测试期间，应该仅收到一个匹配的请求
-            .mount(&mock_server)
-            .await;
+        // Mock::given(header_exists("X-Postmark-Server-Token"))
+        //     .and(header("Content-Type", "application/json"))
+        //     .and(method("POST"))
+        //     .and(SendEmailBodyMatcher) //使用自己定义的匹配，来检查请求的json
+        //     .respond_with(ResponseTemplate::new(200))
+        //     .expect(1) //表示测试期间，应该仅收到一个匹配的请求
+        //     .mount(&mock_server)
+        //     .await;
+	
+	
+	//尝试使用最少的内容，测试能否正常匹配
+	Mock::given(any())
+	    .respond_with(ResponseTemplate::new(200))
+	    .expect(1)
+	    .mount(&mock_server)
+	    .await;
 
         let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
         let subject: String = Sentence(1..2).fake();
         let content: String = Paragraph(1..10).fake();
 
         //执行
-        let _ = email_client
+        let outcome = email_client
             .send_email(subscriber_email, &subject, &content, &content)
             .await;
+	
+	//断言
+	assert_ok!(outcome);
     }
+    
+    ///服务器返回错误
+    #[tokio::test]
+    async fn send_email_fails_if_the_server_returns_500() {
+        //期望发送邮件到base_url
+        let mock_server = MockServer::start().await; //完整的Http服务器,使用一个随机可用的端口
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake())); //将MockServer的URL传递
+
+        // 加入MockServer的mock行为
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(500))
+            .expect(1) //表示测试期间，应该仅收到一个匹配的请求
+            .mount(&mock_server)
+            .await;
+	
+	
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        //执行
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+	
+	//断言
+	assert_err!(outcome);
+    }
+    
+    
+    ///服务器超时错误
+    #[tokio::test]
+    async fn send_email_times_out_if_the_server_takes_too_long() {
+        //期望发送邮件到base_url
+        let mock_server = MockServer::start().await; //完整的Http服务器,使用一个随机可用的端口
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender, Secret::new(Faker.fake())); //将MockServer的URL传递
+
+	
+	//Mockserver 用于回复的response
+	let response = ResponseTemplate::new(200).set_delay(Duration::from_secs(180));
+
+        // 加入MockServer的mock行为
+        Mock::given(any())
+            .respond_with(response)
+            .expect(1) //表示测试期间，应该仅收到一个匹配的请求
+            .mount(&mock_server)
+            .await;
+	
+	
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        //执行
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+	
+	//断言
+	assert_err!(outcome);
+    }
+    
+ 
+
 }
